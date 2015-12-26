d9c2af6110d83b519d877280c0a17fd50291ff8d
Michael Tiller <michael.tiller@gmail.com>
2015-12-26 09:26:17 -0500

    Added onEnter and onExit reactors

--- src/routing.tsx
+++ src/routing.tsx
@@ -2,6 +2,7 @@ import redux = require('redux');
 
 import { DefOp, OpReducer, Operation } from './ops';
 import { SimpleStore } from './store';
+import { Reactor } from './reactors';
 
 // This has a type parameter, but this is "artificial".  It is meant
 // to be used to establish the type of the data associated with this
@@ -37,4 +38,74 @@ export const initialRouteState = {
     params: {}
 }
 
-export const routeReducer = OpReducer(initialRouteState, [setRoute])
+export const routeReducer = OpReducer(initialRouteState, [setRoute]);
+
+function equalParams(a: {}, b: {}): Boolean {
+    // Create arrays of property names
+    var aProps = Object.keys(a);
+    var bProps = Object.keys(b);
+
+    // If number of properties is different,
+    // objects are not equivalent
+    if (aProps.length != bProps.length) {
+        return false;
+    }
+
+    for (var i = 0; i < aProps.length; i++) {
+        var propName = aProps[i];
+
+        // If values of same property are not equal,
+        // objects are not equivalent
+        if (a[propName] !== b[propName]) {
+            return false;
+        }
+    }
+
+    // If we made it this far, objects
+    // are considered equivalent
+    return true;
+}
+
+// This function generates a reactor that is fired when we enter a specified
+// state.
+// N.B. - We are considered to have entered a route if the route is the
+// same but the **parameters** have changed.
+export function onEnter<P>(route: RouteId<P>,
+                           trans: (s: RouteState, // State we are entering
+                                   p: P,
+                                   dispath: redux.Dispatch) => void)
+: Reactor<RouteState> {
+    return (next: RouteState, dispatch: redux.Dispatch, prev: RouteState) => {
+        // If we aren't entering the specified route, there is nothing
+        // to do...
+        if (next.name!=route.id) return;
+
+        // If we are entering the specified route but the parameters are
+        // the same, then there is nothing to do...
+        if (prev.name==next.name && equalParams(prev.params, next.params)) return;
+
+        trans(next, (next.params) as P, dispatch);
+    }
+}
+
+// This function generates a reactor that is fired **AFTER** we have
+// entered a new state that was entered from the specified state.
+// N.B. - We are considered to have exited a route if the route is the
+// same but the **parameters** have changed.
+export function onExit<P>(route: RouteId<P>,
+                           trans: (s: RouteState, // State we are leaving
+                                   p: P,
+                                   dispath: redux.Dispatch) => void)
+: Reactor<RouteState> {
+    return (next: RouteState, dispatch: redux.Dispatch, prev: RouteState) => {
+        // If we aren't leaving the specified route, there is nothing
+        // to do...
+        if (prev.name!=route.id) return;
+
+        // If we are leaving the specified route but the parameters are
+        // the same, then there is nothing to do...
+        if (prev.name==next.name && equalParams(prev.params, next.params)) return;
+
+        trans(prev, (next.params) as P, dispatch);
+    }
+}
